22d6182a9ff94db42be9ad95b6fbf01f4a2868c1
Leonardo Higuti <lyhiguti@gmail.com>
2026-01-20 23:00:44 -0300

    Initial theme

---
+++ src/utils/customTheme.ts
@@ -0,0 +1,15 @@
+'use client';
+
+import { ThemeOptions } from '@mui/material/styles';
+
+export const customTheme: ThemeOptions = {
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#480d1f',
+    },
+    secondary: {
+      main: '#a8593a',
+    },
+  },
+};
